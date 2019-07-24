c2344963befbd5980ddc4e023be6bb0b3f313021
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-07-24 08:47:15 -0600

    react-4-afternoon

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import ClassList from './components/ClassList/ClassList'
+import Student from './components/Student/Student'
+
+export default (
+    <Switch>
+    <Route component={Home} exact path='/'/>
+    <Route comonent={About} path='/about'/>
+    <Route component={ClassList} path='/classlist/:class'/>
+    <Route component={Student} path ='/student/:id'/>
+    </Switch>
+)
+
+
+
+
+
+
+
+
+
+
+
